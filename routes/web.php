a7e158bd33d357a12d6596093ae30f451a041189
Mksoikot <mksoikotbhuiyan117@gmail.com>
2022-06-26 15:52:45 +0600

    directory change

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Teacher');
 });
 
 Route::get('/ajax',"TeacherController@Index");
